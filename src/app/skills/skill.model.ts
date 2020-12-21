6822961c139c5e0152b38666606a33603a93cffc
Caleb Wells <calebcwells@outlook.com>
2020-12-21 14:52:43 -0600

    Updating skills list

--- src/app/skills/skill.model.ts
+++ src/app/skills/skill.model.ts
@@ -1,5 +1,4 @@
 ﻿export interface ISkill {
-    type: string;
     image: string;
     title: string;
     fullUrl: string;
@@ -8,269 +7,304 @@
 
 export const primarySkills: ISkill[] = [
     {
-        type: 'Primary',
-        image: 'assets/images/skills/skill-dotnet.jpg',
-        title: '.NET',
-        fullUrl: 'https://www.microsoft.com/net',
-        friendlyUrl: 'microsoft.com/net'
+        image: 'assets/images/skills/skill-dotnetcore.jpg',
+        title: '.NET Core',
+        fullUrl: 'https://dotnet.microsoft.com/',
+        friendlyUrl: 'dotnet.microsoft.com'
     },
     {
-        type: 'Primary',
+        image: 'assets/images/skills/skill-csharp.jpg',
+        title: 'C#',
+        fullUrl: 'https://docs.microsoft.com/en-us/dotnet/csharp/',
+        friendlyUrl: 'microsoft.com/csharp'
+    },
+    {
         image: 'assets/images/skills/skill-angular.jpg',
         title: 'Angular',
         fullUrl: 'https://angular.io/',
         friendlyUrl: 'angular.io'
     },
     {
-        type: 'Primary',
         image: 'assets/images/skills/skill-typescript.jpg',
         title: 'TypeScript',
         fullUrl: 'https://www.typescriptlang.org/',
         friendlyUrl: 'typescriptlang.org'
     },
     {
-        type: 'Primary',
-        image: 'assets/images/skills/skill-ps.jpg',
-        title: 'Photoshop',
-        fullUrl: 'https://www.adobe.com/products/photoshop.html',
-        friendlyUrl: 'adobe.com/photoshop'
+        image: 'assets/images/skills/skill-azure.jpg',
+        title: 'Azure',
+        fullUrl: 'https://azure.microsoft.com/',
+        friendlyUrl: 'azure.microsoft.com'
     },
     {
-        type: 'Primary',
+        image: 'assets/images/skills/skill-blazor.jpg',
+        title: 'Blazor',
+        fullUrl: 'https://dotnet.microsoft.com/apps/aspnet/web-apps/blazor',
+        friendlyUrl: 'microsoft.com/blazor'
+    },
+    {
+        image: 'assets/images/skills/skill-ef.jpg',
+        title: 'Entity Framework',
+        fullUrl: 'https://docs.microsoft.com/ef/',
+        friendlyUrl: 'microsoft.com/ef'
+    },
+    {
+        image: 'assets/images/skills/skill-powerbi.jpg',
+        title: 'Power BI',
+        fullUrl: 'https://powerbi.microsoft.com',
+        friendlyUrl: 'powerbi.microsoft.com'
+    },
+    {
         image: 'assets/images/skills/skill-rxjs.jpg',
         title: 'RxJS',
-        fullUrl: 'https://reactivex.io/rxjs',
-        friendlyUrl: 'reactivex.io/rxjs'
+        fullUrl: 'https://rxjs-dev.firebaseapp.com/',
+        friendlyUrl: 'rxjs-dev.com'
     },
     {
-        type: 'Primary',
-        image: 'assets/images/skills/skill-webpack.jpg',
-        title: 'Webpack',
-        fullUrl: 'https://webpack.js.org',
-        friendlyUrl: 'webpack.js.org'
-    },
-    {
-        type: 'Primary',
-        image: 'assets/images/skills/skill-html5.jpg',
-        title: 'HTML 5',
-        fullUrl: 'https://www.w3.org/TR/html5/',
-        friendlyUrl: 'w3.org/html5'
-    },
-    {
-        type: 'Primary',
-        image: 'assets/images/skills/skill-css3.jpg',
-        title: 'CSS 3',
-        fullUrl: 'https://www.w3.org/TR/2014/REC-css-namespaces-3-20140320/',
-        friendlyUrl: 'w3.org/css3'
-    },
-    {
-        type: 'Primary',
-        image: 'assets/images/skills/skill-javascript.jpg',
-        title: 'JavaScript',
-        fullUrl: 'https://www.javascript.com/',
-        friendlyUrl: 'javascript.com'
-    },
-    {
-        type: 'Primary',
         image: 'assets/images/skills/skill-json.jpg',
         title: 'JSON',
         fullUrl: 'https://www.json.org/',
         friendlyUrl: 'json.org'
     },
     {
-        type: 'Primary',
-        image: 'assets/images/skills/skill-mdl.jpg',
-        title: 'Material Design',
-        fullUrl: 'https://material.io/',
-        friendlyUrl: 'material.io'
+        image: 'assets/images/skills/skill-html5.jpg',
+        title: 'HTML 5',
+        fullUrl: 'https://www.w3.org/TR/html5/',
+        friendlyUrl: 'w3.org/html5'
     },
     {
-        type: 'Primary',
-        image: 'assets/images/skills/skill-bootstrap.jpg',
-        title: 'Bootstrap',
-        fullUrl: 'https://getbootstrap.com/',
-        friendlyUrl: 'getbootstrap.com'
+        image: 'assets/images/skills/skill-css3.jpg',
+        title: 'CSS 3',
+        fullUrl: 'https://www.w3.org/Style/CSS/Overview.en.html',
+        friendlyUrl: 'w3.org/css'
     },
     {
-        type: 'Primary',
-        image: 'assets/images/skills/skill-ef.jpg',
-        title: 'Entity Framework',
-        fullUrl: 'https://docs.microsoft.com/en-us/ef/',
-        friendlyUrl: 'docs.microsoft.com/ef'
+        image: 'assets/images/skills/skill-javascript.jpg',
+        title: 'JavaScript',
+        fullUrl: 'https://www.javascript.com/',
+        friendlyUrl: 'javascript.com'
     },
     {
-        type: 'Primary',
-        image: 'assets/images/skills/skill-sql.jpg',
-        title: 'SQL',
-        fullUrl: 'https://www.microsoft.com/en-us/sql-server/',
-        friendlyUrl: 'microsoft.com/sql-server'
-    },
-    {
-        type: 'Primary',
         image: 'assets/images/skills/skill-npm.jpg',
         title: 'NPM',
         fullUrl: 'https://www.npmjs.com',
         friendlyUrl: 'npmjs.com'
     },
     {
-        type: 'Primary',
-        image: 'assets/images/skills/skill-git.jpg',
-        title: 'Git',
-        fullUrl: 'https://git-scm.com/',
-        friendlyUrl: 'git-scm.com'
+        image: 'assets/images/skills/skill-ps.jpg',
+        title: 'Photoshop',
+        fullUrl: 'https://www.adobe.com/products/photoshop.html',
+        friendlyUrl: 'adobe.com/photoshop'
     },
     {
-        type: 'Primary',
-        image: 'assets/images/skills/skill-sourcetree.jpg',
-        title: 'SourceTree',
-        fullUrl: 'https://www.sourcetreeapp.com/',
-        friendlyUrl: 'sourcetreeapp.com'
+        image: 'assets/images/skills/skill-bootstrap.jpg',
+        title: 'Bootstrap',
+        fullUrl: 'https://getbootstrap.com/',
+        friendlyUrl: 'getbootstrap.com'
     },
     {
-        type: 'Primary',
-        image: 'assets/images/skills/skill-github.jpg',
-        title: 'Github',
-        fullUrl: 'https://github.com/',
-        friendlyUrl: 'github.com'
+        image: 'assets/images/skills/skill-mdl.jpg',
+        title: 'Material Design',
+        fullUrl: 'https://material.io/',
+        friendlyUrl: 'material.io'
     },
     {
-        type: 'Primary',
-        image: 'assets/images/skills/skill-azure.jpg',
-        title: 'Azure',
-        fullUrl: 'https://azure.microsoft.com/',
-        friendlyUrl: 'azure.microsoft.com'
-    }
-];
-
-export const secondarySkills: ISkill[] = [
-    {
-        type: 'Secondary',
-        image: 'assets/images/skills/skill-xml.jpg',
-        title: 'XML',
-        fullUrl: 'https://www.w3.org/XML/',
-        friendlyUrl: 'w3.org/XML'
-    },
-    {
-        type: 'Secondary',
-        image: 'assets/images/skills/skill-jquery.jpg',
-        title: 'jQuery',
-        fullUrl: 'https://jquery.com/',
-        friendlyUrl: 'jquery.com'
-    },
-    {
-        type: 'Secondary',
-        image: 'assets/images/skills/skill-node.jpg',
-        title: 'Node.js',
-        fullUrl: 'https://nodejs.org/',
-        friendlyUrl: 'nodejs.org'
-    },
-    {
-        type: 'Secondary',
-        image: 'assets/images/skills/skill-react.jpg',
-        title: 'React',
-        fullUrl: 'https://facebook.github.io/react/',
-        friendlyUrl: 'facebook.github.io/react'
-    },
-    {
-        type: 'Secondary',
-        image: 'assets/images/skills/skill-aurelia.jpg',
-        title: 'Aurelia',
-        fullUrl: 'https://aurelia.io/',
-        friendlyUrl: 'aurelia.io'
-    },
-    {
-        type: 'Secondary',
-        image: 'assets/images/skills/skill-breeze.jpg',
-        title: 'Breeze',
-        fullUrl: 'https://www.getbreezenow.com/',
-        friendlyUrl: 'getbreezenow.com'
-    },
-    {
-        type: 'Secondary',
-        image: 'assets/images/skills/skill-flux.jpg',
-        title: 'Flux',
-        fullUrl: 'https://facebook.github.io/flux/',
-        friendlyUrl: 'facebook.github.io/flux'
-    },
-    {
-        type: 'Secondary',
-        image: 'assets/images/skills/skill-knockout.jpg',
-        title: 'Knockout',
-        fullUrl: 'https://knockoutjs.com/',
-        friendlyUrl: 'knockoutjs.com'
-    },
-    {
-        type: 'Secondary',
-        image: 'assets/images/skills/skill-less.jpg',
-        title: 'Less',
-        fullUrl: 'https://lesscss.org/',
-        friendlyUrl: 'lesscss.org'
-    },
-    {
-        type: 'Secondary',
         image: 'assets/images/skills/skill-sass.jpg',
         title: 'Sass',
         fullUrl: 'https://sass-lang.com/',
         friendlyUrl: 'sass-lang.com'
     },
     {
-        type: 'Secondary',
-        image: 'assets/images/skills/skill-aws.jpg',
-        title: 'AWS',
-        fullUrl: 'https://aws.amazon.com/',
-        friendlyUrl: 'aws.amazon.com'
+        image: 'assets/images/skills/skill-dotnet.jpg',
+        title: '.NET',
+        fullUrl:
+            'https://dotnet.microsoft.com/learn/dotnet/what-is-dotnet-framework',
+        friendlyUrl: 'microsoft.com/dotnet'
     },
     {
-        type: 'Secondary',
-        image: 'assets/images/skills/skill-docker.jpg',
-        title: 'Docker',
-        fullUrl: 'https://www.docker.com/',
-        friendlyUrl: 'docker.com'
+        image: 'assets/images/skills/skill-sql.jpg',
+        title: 'SQL',
+        fullUrl: 'https://www.microsoft.com/en-us/sql-server/',
+        friendlyUrl: 'microsoft.com/sql-server'
     },
     {
-        type: 'Secondary',
-        image: 'assets/images/skills/skill-bower.jpg',
-        title: 'Bower',
-        fullUrl: 'https://bower.io/',
-        friendlyUrl: 'bower.io'
+        image: 'assets/images/skills/skill-git.jpg',
+        title: 'Git',
+        fullUrl: 'https://git-scm.com/',
+        friendlyUrl: 'git-scm.com'
     },
     {
-        type: 'Secondary',
-        image: 'assets/images/skills/skill-gulp.jpg',
-        title: 'Gulp',
-        fullUrl: 'https://gulpjs.com/',
-        friendlyUrl: 'gulpjs.com'
+        image: 'assets/images/skills/skill-github.jpg',
+        title: 'Github',
+        fullUrl: 'https://github.com/',
+        friendlyUrl: 'github.com'
     },
     {
-        type: 'Secondary',
+        image: 'assets/images/skills/skill-gitkraken.jpg',
+        title: 'GitKraken',
+        fullUrl: 'https://www.gitkraken.com/',
+        friendlyUrl: 'gitkraken.com'
+    },
+    {
         image: 'assets/images/skills/skill-jira.jpg',
         title: 'JIRA',
         fullUrl: 'https://www.atlassian.com/software/jira/',
         friendlyUrl: 'atlassian.com/jira'
     },
     {
-        type: 'Secondary',
+        image: 'assets/images/skills/skill-confluence.jpg',
+        title: 'Confluence',
+        fullUrl: 'https://www.atlassian.com/software/confluence',
+        friendlyUrl: 'atlassian.com/confluence'
+    }
+];
+
+export const secondarySkills: ISkill[] = [
+    {
+        image: 'assets/images/skills/skill-docker.jpg',
+        title: 'Docker',
+        fullUrl: 'https://www.docker.com/',
+        friendlyUrl: 'docker.com'
+    },
+    {
+        image: 'assets/images/skills/skill-aws.jpg',
+        title: 'AWS',
+        fullUrl: 'https://aws.amazon.com/',
+        friendlyUrl: 'aws.amazon.com'
+    },
+    {
+        image: 'assets/images/skills/skill-fsharp.jpg',
+        title: 'F#',
+        fullUrl: 'https://fsharp.org/',
+        friendlyUrl: 'fsharp.org'
+    },
+    {
+        image: 'assets/images/skills/skill-xamarin.jpg',
+        title: 'Xamarin',
+        fullUrl: 'https://dotnet.microsoft.com/apps/xamarin',
+        friendlyUrl: 'microsoft.com/xamarin'
+    },
+    {
+        image: 'assets/images/skills/skill-python.jpg',
+        title: 'Python',
+        fullUrl: 'https://www.python.org/',
+        friendlyUrl: 'python.org'
+    },
+    {
+        image: 'assets/images/skills/skill-react.jpg',
+        title: 'React',
+        fullUrl: 'https://reactjs.org/',
+        friendlyUrl: 'reactjs.org'
+    },
+    {
+        image: 'assets/images/skills/skill-graphql.jpg',
+        title: 'GraphQL',
+        fullUrl: 'https://graphql.org/',
+        friendlyUrl: 'graphql.org'
+    },
+    {
+        image: 'assets/images/skills/skill-nextjs.jpg',
+        title: 'Next.js',
+        fullUrl: 'https://nextjs.org/',
+        friendlyUrl: 'nextjs.org'
+    },
+    {
+        image: 'assets/images/skills/skill-googlecloud.jpg',
+        title: 'Google Cloud',
+        fullUrl: 'https://cloud.google.com/',
+        friendlyUrl: 'cloud.google.com'
+    },
+    {
+        image: 'assets/images/skills/skill-firebase.jpg',
+        title: 'Firebase',
+        fullUrl: 'https://firebase.google.com/',
+        friendlyUrl: 'firebase.google.com'
+    },
+    {
+        image: 'assets/images/skills/skill-vue.jpg',
+        title: 'Vue.js',
+        fullUrl: 'https://vuejs.org/',
+        friendlyUrl: 'vuejs.org'
+    },
+    {
+        image: 'assets/images/skills/skill-node.jpg',
+        title: 'Node.js',
+        fullUrl: 'https://nodejs.org/',
+        friendlyUrl: 'nodejs.org'
+    },
+    {
+        image: 'assets/images/skills/skill-mongodb.jpg',
+        title: 'MongoDB',
+        fullUrl: 'https://www.mongodb.com/',
+        friendlyUrl: 'mongodb.com'
+    },
+    {
+        image: 'assets/images/skills/skill-less.jpg',
+        title: 'Less',
+        fullUrl: 'http://lesscss.org/',
+        friendlyUrl: 'lesscss.org'
+    },
+    {
+        image: 'assets/images/skills/skill-yarn.jpg',
+        title: 'Yarn',
+        fullUrl: 'https://yarnpkg.com/',
+        friendlyUrl: 'yarnpkg.com'
+    },
+    {
+        image: 'assets/images/skills/skill-webpack.jpg',
+        title: 'Webpack',
+        fullUrl: 'https://webpack.js.org',
+        friendlyUrl: 'webpack.js.org'
+    },
+    {
+        image: 'assets/images/skills/skill-bower.jpg',
+        title: 'Bower',
+        fullUrl: 'https://bower.io/',
+        friendlyUrl: 'bower.io'
+    },
+    {
+        image: 'assets/images/skills/skill-gulp.jpg',
+        title: 'Gulp',
+        fullUrl: 'https://gulpjs.com/',
+        friendlyUrl: 'gulpjs.com'
+    },
+    {
+        image: 'assets/images/skills/skill-jquery.jpg',
+        title: 'jQuery',
+        fullUrl: 'https://jquery.com/',
+        friendlyUrl: 'jquery.com'
+    },
+    {
         image: 'assets/images/skills/skill-bitbucket.jpg',
         title: 'Bitbucket',
         fullUrl: 'https://www.atlassian.com/software/bitbucket/',
         friendlyUrl: 'atlassian.com/bitbucket'
     },
     {
-        type: 'Secondary',
+        image: 'assets/images/skills/skill-sourcetree.jpg',
+        title: 'SourceTree',
+        fullUrl: 'https://www.sourcetreeapp.com/',
+        friendlyUrl: 'sourcetreeapp.com'
+    },
+    {
+        image: 'assets/images/skills/skill-xml.jpg',
+        title: 'XML',
+        fullUrl: 'https://www.w3.org/XML/',
+        friendlyUrl: 'w3.org/XML'
+    },
+    {
         image: 'assets/images/skills/skill-apache.jpg',
         title: 'Apache',
         fullUrl: 'https://www.apache.org/',
         friendlyUrl: 'apache.org'
     },
     {
-        type: 'Secondary',
         image: 'assets/images/skills/skill-mysql.jpg',
         title: 'MySQL',
         fullUrl: 'https://www.mysql.com/',
         friendlyUrl: 'mysql.com'
     },
     {
-        type: 'Secondary',
         image: 'assets/images/skills/skill-nhibernate.jpg',
         title: 'NHibernate',
         fullUrl: 'https://nhibernate.info/',
